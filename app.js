80f31480e1f9dd1114b46d47458435b180ba96e4
INTEL <yousufzubair1234@gmail.com>
2022-10-18 06:38:26 +0500

    Created a new branch of mongoose and changed some of its code

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017/';
+const dbName = 'fruitsDB';
 
-// Database Name
-const dbName = 'myproject';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
-
+mongoose.connect(url + dbName)
 
 const insertDocuments = function(db, callback) {
   // Get the documents collection
@@ -43,9 +27,22 @@ const insertDocuments = function(db, callback) {
     }
   ], function(err, result) {
     assert.equal(err, null);
-    assert.equal(3, result.result.n); 
-    assert.equal(3, result.ops.length);
+    // assert.equal(3, result.result.n); 
+    // assert.equal(3, result.ops.length);
     console.log("Inserted 3 documents into the collection");
     callback(result);
+  });
+}
+
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
   });
 }
